1d0d72c21c6ef39933eb41692f4c1be9d2a5b051
Joshua Gato <joshuagato37@gmail.com>
2021-08-11 12:22:40 +0000

    Wrote functions to build and display the initial(input) matrix

--- script.js
+++ script.js
@@ -8,6 +8,23 @@ const fetchMatrix = async () => {
     const response = await fetch('./matrix.csv', options);
     const data = await response.text();
     const matrixArray = data.split('\n');
+
+    const matrix = buildMatrix(matrixArray);
+    displayCSVInput(matrix);
+};
+
+// Function to build the CSV file values into a matrix
+const buildMatrix = (elementsArray) => {
+    const [topRow, middleRow, bottomRow] = elementsArray;
+    return `${topRow}
+        ${middleRow}
+        ${bottomRow}
+    `;
+};
+
+// Function to display the input of the matrix
+const displayCSVInput = (matrix) => {
+    document.querySelector('#csv-input').value = matrix;
 };
 
 await fetchMatrix();
